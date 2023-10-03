8a8ed8a73d3c032ca419df1f44f5bb085b315818
Hengbro <hengkisaoremaha@gmail.com>
2023-10-04 04:03:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/zenatapos/ui/auth/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.zenatapos.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.zenatapos.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/zenatapos/ui/auth/RegisterActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zenatapos.ui.auth
+
+class RegisterActivity {
+
+}
